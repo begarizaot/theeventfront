f5869cc3e5eca6e3b76e55676c309012dde3d8c3
t1bgarizao <bgarizao@todo1.net>
2025-01-02 17:29:36 -0500

    ajustes

--- src/ui/components/LogoLink/index.tsx
+++ src/ui/components/LogoLink/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 // import { environment } from "../../../environments/environment";
+import logo from "../../../assets/bg-white.png";
 
 interface LogoLinkProps {
   classLink?: string;
@@ -11,7 +12,7 @@ export const LogoLink = ({ className, classLink, div }: LogoLinkProps) => {
   const image = () => {
     return (
       <img
-        src={`https://www.theeventjet.com/assets/images/bg-white.png`}
+        src={logo}
         alt=""
         className={`w-6 sm:w-4 lg:w-3 ${className ? className : ""}`}
       />
--- src/ui/components/MetaComp/index.tsx
+++ src/ui/components/MetaComp/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { Helmet } from "react-helmet";
 
 interface MetaProps {
@@ -8,24 +7,6 @@ interface MetaProps {
 }
 
 export const MetaComp = ({ image, title, url }: MetaProps) => {
-  useEffect(() => {
-    if (title) document.title = title;
-
-    const setMetaTag = (property:any, content:any) => {
-        let metaTag = document.querySelector(`meta[property='${property}']`);
-        if (!metaTag) {
-            metaTag = document.createElement("meta");
-            metaTag.setAttribute("property", property);
-            document.head.appendChild(metaTag);
-        }
-        metaTag.setAttribute("content", content);
-    };
-
-    if (image) setMetaTag("og:image", image);
-    if (url) setMetaTag("og:url", url);
-    if (title) setMetaTag("og:title", title);
-}, [title, image, url]);
-
   return (
     <Helmet>
       <meta charSet="UTF-8" />
