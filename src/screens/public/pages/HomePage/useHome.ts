3733006ef5dd7143132b29283de19ee63e44c91a
begarizaot <brayan@theeventjet.com>
2025-07-14 00:10:36 -0500

    ajustes de estilos

--- src/screens/public/pages/HomePage/useHome.ts
+++ src/screens/public/pages/HomePage/useHome.ts
@@ -1,12 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { AppDispatch, RootState } from "../../../../store";
 import { getArtist, getEventHome, getHome } from "../../../../store/thunks";
+import { getLocalStorage } from "../../../../hooks";
 
 export const useHome = () => {
   const dispatch: AppDispatch = useDispatch();
-  const { homeDate, loading: loadingHome } = useSelector(
+  const { homeDate:home, loading: loadingHome } = useSelector(
     (state: RootState) => state.home
   );
   const { eventDate, loading: loadingEvents } = useSelector(
@@ -16,11 +17,22 @@ export const useHome = () => {
     (state: RootState) => state.artist
   );
 
+  const [homeDate, setHomeDate] = useState({})
+
   useEffect(() => {
     dispatch(getHome());
     dispatch(getEventHome());
     dispatch(getArtist());
   }, [dispatch]);
+
+  useEffect(() => {
+    fechHomeData();
+  }, [home]);
+
+  const fechHomeData = async () => {
+    const res = await getLocalStorage("home");
+    setHomeDate(home ?? res ?? {});
+  };
 
   return {
     homeDate,
