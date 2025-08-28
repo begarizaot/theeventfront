7364a24c2fff263f45cf66823c7e54456182c828
begarizaot <brayan@theeventjet.com>
2025-08-28 16:10:17 -0500

    ajustes de diseno

--- src/screens/public/pages/EventDetailsPage/components/Details.tsx
+++ src/screens/public/pages/EventDetailsPage/components/Details.tsx
@@ -22,51 +22,6 @@ export const DetailsComp = ({ dataEvent }: DescriptionCompProps) => {
               <h2 className="text-2xl bebasNeue">Description</h2>
               <ComDescription contenido={dataEvent?.description} />
             </div>
-            {/* hours */}
-            {/* <div className="grid gap-1">
-              <h2 className="text-2xl bebasNeue">Hours</h2>
-              <p className="text-sm">{`${useMoment(
-                dataEvent?.start_date
-              ).format("hh:mm a")} - ${useMoment(dataEvent?.end_date).format(
-                "hh:mm a"
-              )}`}</p>
-            </div> */}
-            {/* contact */}
-            <div className="grid gap-1">
-              <h2 className="text-2xl bebasNeue">Organizers</h2>
-              <div className="flex gap-2">
-                <Link
-                  to={`tel:${dataEvent?.users_id?.country_id?.code}${dataEvent?.users_id?.phoneNumber}`}
-                  className="bg-white px-2 py-[3px] rounded-full flex items-center"
-                >
-                  <TextPrimary
-                    label={
-                      <>
-                        <span className="pi pi-phone text-[10px]"></span>
-                        {dataEvent?.users_id?.country_id?.code}{" "}
-                        {dataEvent?.users_id?.phoneNumber}
-                      </>
-                    }
-                    className="text-xs gap-2 flex items-center"
-                  />
-                </Link>
-
-                <Link
-                  to={`mailto:${dataEvent?.users_id?.email}`}
-                  className="bg-white px-2 py-[3px] rounded-full flex items-center"
-                >
-                  <TextPrimary
-                    label={
-                      <>
-                        <span className="pi pi-envelope text-[10px]"></span>
-                        {dataEvent?.users_id?.email}
-                      </>
-                    }
-                    className="text-xs gap-2 flex items-center"
-                  />
-                </Link>
-              </div>
-            </div>
           </div>
           {(dataEvent?.urls_images_advertising ?? [])?.length > 0 && (
             <div className="col-span-1 gap-2 hidden sm:flex flex-col">
