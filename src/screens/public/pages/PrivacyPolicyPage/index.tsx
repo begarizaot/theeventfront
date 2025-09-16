958b843914e966e6081a40465b321c596991a2e8
begarizaot <brayan@theeventjet.com>
2025-09-16 08:59:12 -0500

    terminos y condiciones

--- src/screens/public/pages/PrivacyPolicyPage/index.tsx
+++ src/screens/public/pages/PrivacyPolicyPage/index.tsx
@@ -1,22 +1,36 @@
-import { useEffect, useState } from "react";
-import { getLocalStorage } from "../../../../hooks";
+import { useEffect } from "react";
+import { Skeleton } from "antd";
+import { useDispatch, useSelector } from "react-redux";
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 
+import { getPolicy } from "../../../../store/thunks";
+import { AppDispatch, RootState } from "../../../../store";
+
 export const PrivacyPolicyPage = () => {
-  const [data, setData] = useState([]);
+  const dispatch: AppDispatch = useDispatch();
+  const { policyDate, loading } = useSelector(
+    (state: RootState) => state.polity
+  );
 
   useEffect(() => {
-    fechPolicyData();
-  }, []);
-
-  const fechPolicyData = async () => {
-    const res = await getLocalStorage("policy");
-    setData(res?.privacyPolicy ?? []);
-  };
+    dispatch(getPolicy());
+  }, [dispatch]);
 
   return (
     <div className="pt-16 lg:pt-12 w-full mx-auto max-w-[80rem] min-h-screen z-10 relative pb-10 px-4 sm:px-8">
-      <BlocksRenderer content={data} />
+      {loading && (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {Array.from({ length: 20 }).map((_, index) => (
+            <div key={index} className="col-span-1 h-6">
+              <Skeleton.Node
+                active
+                className="bg-white/20 w-full! rounded-sm h-full!"
+              />
+            </div>
+          ))}
+        </div>
+      )}
+      {!loading && <BlocksRenderer content={policyDate?.privacyPolicy ?? []} />}
     </div>
   );
 };
