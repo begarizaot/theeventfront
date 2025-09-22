73567477f016963d7e8f1ac8771af452f5ebb585
begarizaot <brayan@theeventjet.com>
2025-09-21 19:56:41 -0500

    ajustes de texto

--- src/screens/admin/pages/EventDetailsPage/index.tsx
+++ src/screens/admin/pages/EventDetailsPage/index.tsx
@@ -52,7 +52,7 @@ export const EventDetailsPage = () => {
                     navigate(`/editEvent/${eventData?.id_event}`);
                   }}
                 >
-                  Edit Event
+                  Save Event
                 </Button>
               </>
             )}
